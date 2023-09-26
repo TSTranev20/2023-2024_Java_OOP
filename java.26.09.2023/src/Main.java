96f3af2b36c7c63850f5db58682afe8877b6ad07
Трани Стоянов Транев <TSTranev20@codingburgas.bg>
2023-09-26 10:19:40 +0300

    26.09class

---
+++ java.26.09.2023/src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double fResult = addTwoNumbersAndDevideThenToTwo(10, 2.5);
+        double sResult = addTwoNumbersAndDevideThenToTwo(100, addTwoNumbersAndDevideThenToTwo(1000, 10.4));
+         System.out.println(fResult);
+         System.out.println(sResult);
+    }
+
+    public static double addTwoNumbersAndDevideThenToTwo(int f, double s)
+    {
+        double result = f + s;
+        return result;
+    }
+}
+
+/*
+
+
+
+
+
+
+
+
+
+import java.util.Scanner;
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        printTest();
+        printText("Ivan");
+        printText("Oragan");
+        printText("Petkan");
+    }
+
+    public static void printText(String text)
+    {
+        int a = 5;
+        System.out.println(text + " " + a);
+        ++a;
+    }
+
+    public static void printTest()
+    {
+        System.out.println("test");
+        test();
+    }
+
+    public static void test()
+    {
+        System.out.println("testing...");
+    }
+}
+
+ */
