39be5dd25b1e36acc7c5f90ef15abb34eaeec7fc
Трани Стоянов Транев <TSTranev20@codingburgas.bg>
2023-10-10 13:00:05 +0300

    Task4

---
+++ task4.10.10.2023/src/task2/Main.java
@@ -0,0 +1,22 @@
+package task2;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        System.out.println(factorial(n));
+    }
+    public static int factorial(int n)
+    {
+        if(n > 1)
+        {
+            return n * factorial(n-1);
+        }
+        else {
+            return 1;
+        }
+    }
+}
