495e048b184bcad0105ae4e08dff18c759648312
TSTranev20 <TSTranev20@codingbvurgas.bg>
2024-03-13 10:43:53 +0200

    Java 13.03

---
+++ java.13.03/java13.03/src/Main.java
@@ -0,0 +1,34 @@
+
+
+
+/*
+public class Main {
+
+    public static void main(String[] args) {
+        Set<String> names = new LinkedHashSet<>();
+
+        names.add("Zhivko");
+        names.add("Ivan");
+        names.add("Viktor");
+
+        names.forEach (e -> System.out.println(e));
+
+
+    }
+}
+  */
+/* System.out.println(names.get(2));
+
+        Map<String, Double> grades = new LinkedHashMap<>();
+
+        grades.put("Ivan", 5.50);
+        grades.put("Vik", 4.50);
+
+        Set<String> keys = grades.keySet();
+
+        for(String key : keys){
+            System.out.println(key + " - " + grades.get(key));
+        }
+
+        Collection<Double> value = grades.values();
+  */
